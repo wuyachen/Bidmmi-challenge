481a7ba93d62f6fe5cf386dd33171031131464e8
Yachen Wu <wuy176@mcmaster.ca>
2020-07-07 15:53:27 -0400

    last

--- insert.php
+++ insert.php
@@ -1,11 +1,15 @@
 <?php
 $data_file = fopen("example.txt","w");
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-$email = $_POST['email'];
-$text = $username . " " . $password . " " . " " . $email;
-
-fwrite($data_file, $text);
-fclose($data_file);
+if (!empty($username) || !empty($password) || !empty($email)) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+    $text = $username . " " . $password . " " . " " . $email;
+    fwrite($data_file, $text);
+    fclose($data_file);
+    echo" New record created sucessfully";}
+else{
+    echo "All field are required";
+    die();
+}
 ?>
